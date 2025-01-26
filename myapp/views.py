f8a51fd335400cbaefcfe1b55374abfb6325e303
Ardiantohendy <bayuardianto421@gmail.com>
2025-01-26 15:07:10 +0700

    new commit

--- myapp/views.py
+++ myapp/views.py
@@ -3,4 +3,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'name': 'Watanabe Toru',
+        'age' : 27,
+        'job' : 'Debugger',
+        'company': 'Sun Microsystem',
+        'status' : 'Contract'
+    }
+    return render(request, 'index.html', context)
+
+def counter(request):
+    text = request.GET['text']
+    words = len(text.replace(' ', '')) 
+    
+    return render(request, 'counter.html', {'words': words})
+

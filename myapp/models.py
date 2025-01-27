e9dbf515ce4cd2dfda5467a1ccc31656871b0302
Ardiantohendy <bayuardianto421@gmail.com>
2025-01-28 00:13:45 +0700

    new commit

--- myapp/models.py
+++ myapp/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Feature:
-    id = int
-    title = str
-    desc = str
-    icon = str
+    def __init__(self, id: int, title: str, desc: str, icon: str):
+        self.id = id
+        self.title = title
+        self.desc = desc
+        self.icon = icon
--- myapp/views.py
+++ myapp/views.py
@@ -4,26 +4,11 @@ from .models import Feature
 
 # Create your views here.
 def index(request):
-    feature1 = Feature()
-    feature1.id = 1
-    feature1.title = "Layanan Kami Cepat"
-    feature1.desc = "pemesanan makanan kami dibuat oleh chef ternama sehingga penyajiannya cepat dan juga berkualitas"
-    feature1.icon = "bi bi-clipboard-data"
-
-    feature2 = Feature()
-    feature2.id = 2
-    feature2.title = "Layanan Kami Berkualitas"
-    feature2.desc = "Semua masakkan kami berkualitas tinggi karena dimasak oleh para chef yang ahli dalam bidangnya"
-    feature2.icon = "bi bi-gem"
-
-    feature3 = Feature()
-    feature3.id = 3
-    feature3.title = "Dapat Dipesan Secara Daring"
-    feature3.desc = "Semua menu kami dapat dipesan secara daring melalui aplikasi Yummy yang tersedia di Playstore dan Appstore"
-    feature3.icon = "bi bi-inboxes"
+    feature1 = Feature(1, "Layanan Kami Cepat", "pemesanan makanan kami dibuat oleh chef ternama sehingga penyajiannya cepat dan juga berkualitas", "bi bi-clipboard-data")
+    feature2 = Feature(2, "Layanan Kami Berkualitas", "Semua masakkan kami berkualitas tinggi karena dimasak oleh para chef yang ahli dalam bidangnya", "bi bi-gem")
+    feature3 = Feature(3, "Dapat Dipesan Secara Daring", "Semua menu kami dapat dipesan secara daring melalui aplikasi Yummy yang tersedia di Playstore dan Appstore", "bi bi-inboxes")
 
     features = [feature1, feature2, feature3]
-
 
     return render(request, 'index.html', {'features' : features})
 
